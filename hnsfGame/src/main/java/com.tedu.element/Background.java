9054999310d62607e0b0316b0aa3d37e0b04d7b8
Sherrijy <2951898130@qq.com>
2025-07-18 22:50:50 +0800

    cjy

---
+++ hnsfGame/src/main/java/com.tedu.element/Background.java
@@ -0,0 +1,30 @@
+package com.tedu.element;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Background extends ElementObj{
+
+
+
+    public Background(int x, int y, int w, int h, ImageIcon icon) {
+       super(x, y, w, h, icon);
+    }
+
+    @Override
+    public void showElement(Graphics g) {
+
+        g.drawImage(this.getIcon().getImage(),this.getX(),this.getY(),this.getW(),this.getH(),null);
+    }
+
+    @Override
+    public void KeyClick(boolean isClicked, int key) {}
+    @Override
+    public void add(long gameTime) {}
+    @Override
+    public ElementObj createElement(String str) {
+        return this;
+    }
+
+
+}
---
+++ hnsfGame/src/main/java/com.tedu.element/Wave.java
@@ -0,0 +1,56 @@
+package com.tedu.element;
+
+import com.tedu.manager.ElementManager;
+import com.tedu.manager.GameElement;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Wave extends ElementObj implements Runnable{
+
+
+    @Override
+    public void setWave(ImageIcon[] waveFade) {
+        this.icon = waveFade[0];
+    }
+
+    public Wave(int i, int j, ImageIcon[] icons){
+        super(i * Boxs.BASE_WIDTH + 5,j * Boxs.BASE_WIDTH + 10,40,40,icons[0]);
+
+    }
+
+    @Override
+    public void showElement(Graphics g) {
+        if(this.icon != null){
+            g.drawImage(this.icon.getImage(),this.getX(),this.getY(),this.getW(),this.getH(),null);
+        }
+
+    }
+
+    @Override
+    public void KeyClick(boolean isClicked, int key) {
+
+    }
+
+    @Override
+    public void updateImage(){
+    }
+    @Override
+    public void run() {
+        try {
+            //沉睡900毫秒后，自动消失
+            Thread.sleep(900);
+            setIcon(null);
+            ElementManager.getManager().getElementsByKey(GameElement.WAVE).remove(this);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void add(long gameTime) {}
+    public ElementObj createElement(String str) {
+        return this;
+    }
+}
